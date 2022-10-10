49195009c8a11840463496965398cf03b303c4c4
Alireza <alirezayoom@gmail.com>
2022-10-10 11:20:31 +0430

    layout and navigation added

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,3 @@
+function FeaturedPost() {}
+
+export default FeaturedPost;
---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>logo</div>;
+}
+
+export default Logo;
