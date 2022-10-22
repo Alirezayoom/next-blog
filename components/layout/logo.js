8756128faae78cf8998bbbb6f983f89f50d697a3
Alireza <alirezayoom@gmail.com>
2022-10-22 09:13:56 +0430

    reset

--- components/layout/logo.js
+++
@@ -1,7 +0,0 @@
-import classes from "./logo.module.css";
-
-function Logo() {
-  return <div className={classes.logo}>logo</div>;
-}
-
-export default Logo;
--- pages/contact.js
+++
@@ -1,5 +0,0 @@
-function Contact() {
-
-}
-
-export default Contact;
