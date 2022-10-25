7e4d8c839df846207498a14cc9ef103a6c1f9841
alireza <alirezayoom@gmail.com>
2022-10-25 09:28:23 +0430

    rendering code snippet from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,44 +1,58 @@
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
-import Image from 'next/image'
-
+import Image from "next/image";
 
 function PostContent(props) {
-  const {post} = props;
+  const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
     // img(image) {
     //   return (
-    //   <Image src={`/images/posts/${post.slug}/${image.src}`} 
-    //   alt={image.alt} 
-    //   width={600} 
-    //   height={300} 
+    //   <Image src={`/images/posts/${post.slug}/${image.src}`}
+    //   alt={image.alt}
+    //   width={600}
+    //   height={300}
     //   />
     //   )
     // },
-    p(paragraph){
-      const {node} = paragraph;
-      if(node.children[0].tagName === 'img'){
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
         const image = node.children[0];
 
-        return <div className={classes.image}>
-          <Image src={`/images/posts/${post.slug}/${image.properties.src}`} 
-          alt={image.alt} 
-          width={600} 
-          height={300} 
-          />
-        </div>
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
       }
-      return <p>{paragraph.children}</p>
-    }
-  }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { language, children } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language="javascript"
+          children={children}
+        />
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader image={imagePath} title={post.title} />
-      <ReactMarkdown components={customRenderers} >{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
