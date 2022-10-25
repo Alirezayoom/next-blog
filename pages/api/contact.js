d85d7fe837856673f1bdf32b0c18d80f24364e3d
alireza <alirezayoom@gmail.com>
2022-10-25 11:17:48 +0430

    data sotred in mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     console.log(name);
@@ -14,13 +16,36 @@ function handler(req, res) {
       return;
     }
 
-    //store in a db
     const newMessage = {
       email,
       name,
       message,
     };
-    console.log(newMessage);
+    //store in a db
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://ali:ali@cluster0.qafjedp.mongodb.net/sms?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      console.log(
+        "errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+      );
+      res.status(500).json({ message: "couold not connect to database" });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.inseretedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "data did not save to database!" });
+      return;
+    }
+    client.close();
+
     res
       .status(201)
       .json({ message: "email sent successfully", message: newMessage });
