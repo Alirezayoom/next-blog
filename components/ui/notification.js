0d658398fe369c23ac399b16d57dfdb0cd754e0e
alireza <alirezayoom@gmail.com>
2022-10-25 15:24:13 +0430

    react portal added

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -17,11 +17,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 }
 
