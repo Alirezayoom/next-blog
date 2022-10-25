4af22ec09dee57203be5f0a2c6fda8c06378dc16
alireza <alirezayoom@gmail.com>
2022-10-25 09:46:33 +0430

    contact form added

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
+import ContactForm from "../components/contact/contact-form";
+
 function ContactPage() {
-  return <h1>contact page</h1>;
+  return <ContactForm />;
 }
 
 export default ContactPage;
