b5f2c12218e1efe953d0f85e5001b75a440d3942
alirezayoom <alirezayoom@gmail.com>
2023-01-16 14:55:24 +0430

    reset

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 };
 
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: "ali",
-        mongodb_password: "ali",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "sms-test",
-      },
-    };
-  }
-
-  return {
-    env: {
-      mongodb_username: "ali",
-      mongodb_password: "ali",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "sms",
-    },
-  };
-};
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,5 @@
-import { Fragment } from "react";
-import Head from "next/head";
-import Hero from "../components/home-page/hero";
-import FeaturedPosts from "../components/home-page/featured-posts";
-import { getFeaturedPosts } from "../lib/posts-util";
-
 function HomePage(props) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Alirezas Blog</title>
-        <meta
-          name="description"
-          content="I post about web development and programming."
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
-  );
-}
-
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
-
-  return {
-    props: {
-      posts: featuredPosts,
-    },
-  };
+  return <div>hi</div>;
 }
 
 export default HomePage;
