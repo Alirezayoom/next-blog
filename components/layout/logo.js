bf708f9d32109450b1f1f0627051c51d67cff521
alirezayoom <alirezayoom@gmail.com>
2023-01-16 17:40:02 +0430

    layout added

---
+++ components/layout/logo.js
@@ -0,0 +1,5 @@
+import classes from "./logo.module.css";
+
+export default function Logo() {
+  return <div className={classes.logo}>NEXT BLOG</div>;
+}
---
+++ components/layout/navbar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Logo from "./logo";
+import classes from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
