057ecfdb6e4440f398f33a77147c40d1f5a6f062
alirezayoom <alirezayoom@gmail.com>
2023-01-17 10:25:17 +0430

    post-grid added

---
+++ components/posts/posts-grid.js
@@ -0,0 +1,12 @@
+import PostItem from "./post-item";
+import classes from "./posts-grid.module.css";
+
+export default function PostsGrid({ posts }) {
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem />
+      ))}
+    </ul>
+  );
+}
