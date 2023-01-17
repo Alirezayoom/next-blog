048b9425d6b7c7c0de9d047ded761ec0d256d34d
alirezayoom <alirezayoom@gmail.com>
2023-01-17 15:28:46 +0430

    post detail added

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import PostHeader from "./post-header";
+
+import classes from "./post-content.module.css";
+
+const dummyPost = {
+  title: "Next JS",
+  slug: "getting-started-with-nextjs",
+  date: "2022-12-12",
+  image: "nextjs.jpeg",
+  content: "# This is a frist post",
+};
+
+export default function PostContent() {
+  const imagePath = `/images/posts/${dummyPost.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={dummyPost.title} image={imagePath} />
+      {dummyPost.content}
+    </article>
+  );
+}
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,1 +1,5 @@
-export default function PostDetailPage() {}
+import PostContent from "../../components/posts/post-detail/post-content";
+
+export default function PostDetailPage() {
+  return <PostContent />;
+}
