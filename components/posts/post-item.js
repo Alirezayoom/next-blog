9bf928cce624d9a1cf314feca960442556553978
alirezayoom <alirezayoom@gmail.com>
2023-01-17 10:44:08 +0430

    post item added

---
+++ components/posts/post-item.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import classes from "./post-item.module.css";
+
+export default function PostItem({ post }) {
+  const { title, image, excerpt, date, slug } = post;
+
+  const humanReadDate = new Date(date).toLocaleString("en-us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200} />
+        </div>
+
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{humanReadDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
+}
