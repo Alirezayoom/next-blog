bc753adfd05bb29a285d80104988d9e5f3d50036
alirezayoom <alirezayoom@gmail.com>
2023-01-18 12:16:41 +0430

    md images rendered with Image

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,46 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
+import Image from "next/image";
 
 import classes from "./post-content.module.css";
 
 export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.image}`;
 
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //   <Image src={`/images/posts/${post.slug}/${image.src}`}
+    //   alt={image.alt}
+    //   width={600}
+    //   height={300}
+    //   />
+    //   )
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -6,6 +6,26 @@ excerpt: "Next JS is a react framework for production - it makes building fullst
 isFeatured: true
 ---
 
-# this is the title
+NextJS is a **framework for ReactJS**
 
-this is a link [link](www.google.com)
+Wait a second ... a 'framework' isn't React itself already a framework for javascript?
+
+Well ... first of all, React is a 'library' for javascript. That is important for some people.
+
+## Why would we then need NextJS?
+
+Because NextJS makes building React apps easier - especially react sould have server-side rendering.
+
+In this article, we'll dive into core concepts and features NextJS has to offer:
+
+- File-based Routing
+- Built-in Page pre-rendering
+- Rich Data Fetching Capabilities
+- Image Optimization
+- Much more
+
+## File based Routing
+
+![Create routes via your file + folder strucutre](nextjs-file-based-routing.png)
+
+... More content ...
