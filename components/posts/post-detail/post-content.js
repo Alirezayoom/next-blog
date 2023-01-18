ea3288ded93e16aea00add6b36dff803ec9dc532
alirezayoom <alirezayoom@gmail.com>
2023-01-18 11:46:32 +0430

    md files rendered

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,13 @@ import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
 
-const dummyPost = {
-  title: "Next JS",
-  slug: "getting-started-with-nextjs",
-  date: "2022-12-12",
-  image: "nextjs.jpeg",
-  content: "# This is a frist post",
-};
-
-export default function PostContent() {
-  const imagePath = `/images/posts/${dummyPost.image}`;
+export default function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={dummyPost.title} image={imagePath} />
-      <ReactMarkdown>{dummyPost.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,30 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-export default function PostDetailPage() {
-  return <PostContent />;
+export default function PostDetailPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const { slug } = context.params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
