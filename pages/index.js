cdbc2d06c01162d8a88e46782b7e0ae1a815373a
alirezayoom <alirezayoom@gmail.com>
2023-01-18 09:56:00 +0430

    md files rendered

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,22 @@
 import FeaturedPosts from "../components/home/featured-posts";
 import Hero from "../components/home/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const dummyPosts = [
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs2",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs3",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs4",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-];
-
-export default function HomePage() {
+export default function HomePage({ featuredPosts }) {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={dummyPosts} />
+      <FeaturedPosts posts={featuredPosts} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      featuredPosts: featuredPosts,
+    },
+  };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,16 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const dummyPosts = [
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs2",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs3",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-  {
-    title: "Next JS",
-    slug: "getting-started-with-nextjs4",
-    excerpt:
-      "Next JS is a react framework for production - it makes building fullstack react with built in SSR",
-    date: "2022-12-12",
-    image: "nextjs.jpeg",
-  },
-];
+export default function PostsPage({ allPosts }) {
+  return <AllPosts posts={allPosts} />;
+}
 
-export default function PostsPage() {
-  return <AllPosts posts={dummyPosts} />;
+export async function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      allPosts: allPosts,
+    },
+  };
 }
